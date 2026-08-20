2caa1b13f2c21436624cb45e4bae37c7e5c8440e
youssefelbaz32 <107543132+youssefelbaz32@users.noreply.github.com>
2026-08-20 00:22:33 -0700

    Add: number-of-one-bits - submission-2

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+
+        cnt = 0
+
+        while n:
+            n &= (n - 1)
+            cnt += 1
+        
+        return cnt
+        # for i in range(32):
+        #     if (n & 1):
+        #         cnt += 1
+        #     n = n >> 1
+        
+        # return cnt
+
+        
